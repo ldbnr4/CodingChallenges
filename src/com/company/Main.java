24d885f9dc35c43817a9a73593373a1e2fa37ec3
Lorenzo Boyice <lorenzo.boyice@garmin.com>
2016-07-08 17:04:13 -0500

    working on reverse challenge

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,12 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         StringArray.rotateArray(new Integer[]{1,2}, 6);
+        StringArray.rotateArray(new Integer[]{1,2}, -5);
         StringArray.rotateArray(new Integer[]{}, 6);
         StringArray.rotateArray(new Integer[]{1,2,3,4,5,6,7,8}, 0);
         StringArray.rotateArray(new Integer[]{1,2,3,4,5,6,7,8}, -3);
         StringArray.rotateArray(new Integer[]{1,2,3,4,5,6,7,8}, 3);
+
+        StringArray.stringReversal("This is a sentence.");
     }
 }
--- src/com/company/StringArray.java
+++ src/com/company/StringArray.java
@@ -2,49 +2,68 @@ package com.company;
 
 import java.util.Arrays;
 
+import static java.lang.Math.abs;
+
 /**
  * Created by Lorenzo on 7/7/2016.
  *
  */
 class StringArray {
-    static void rotateArray(Object[] array, int k){
+    static void rotateArray(Object[] array, int shift){
 
         int length = array.length;
         if (length == 0){
-            System.out.println("Empty array!");
-            System.out.println();
+            System.out.println("Empty array!\n");
             return;
         }
 
-        System.out.println("Before: "+ Arrays.toString(array));
+        System.out.print("Before: "+ Arrays.toString(array));
+        System.out.println(" Shift = "+shift);
 
-        if(k > length){
-            k = k% length;
+        if(abs(shift) > length){
+            shift = shift%length;
         }
 
         Object[] result = new Object[length];
-        if(k > 0) {
-            for (int i = 0, j = 0; i < length; i++) {
-                j = i + k;
-                if (j >= length) {
-                    j = j % length;
-                }
+        for (int oldPosition = 0, newPosition; oldPosition < length; oldPosition++) {
+            newPosition = oldPosition + shift;
+            if (newPosition >= length) {
+                newPosition = newPosition % length;
+            }
+            if(newPosition < 0 ){
+                newPosition += length;
+            }
 
-                result[i] = array[j];
-            }
-        }
-        else{
-            for(int i = length, j = 0; i >= 0; i--){
-                j = i + k;
-                //if (j < 0)
-            }
+            result[newPosition] = array[oldPosition];
         }
 
-        System.out.println("After: "+ Arrays.toString(result));
-        System.out.println();
+        System.out.println("After: "+ Arrays.toString(result)+"\n");
     }
 
-    static void stringReversal(){}
+    static void stringReversal(String sentence){
+        if(sentence.isEmpty()){
+            System.out.println("Empty string!\n");
+        }
+
+        System.out.println("Beginning string: \""+sentence+"\"");
+        char[] chars = sentence.toCharArray();
+        String result = "";
+
+        for(int cursor = chars.length-1, wordStart = chars.length-1; cursor >= 0; cursor--){
+            if (String.valueOf(chars[cursor]).matches("\\W") || cursor == 0){
+                String word;
+                if (wordStart == cursor){
+                    word = String.valueOf(chars[wordStart]);
+                }else {
+                    word = String.valueOf(chars).substring(cursor, wordStart);
+                }
+                result += word;
+                wordStart = cursor;
+            }
+        }
+
+        System.out.println("Reversed string: \""+result+"\"");
+    }
 }
 
 /*
